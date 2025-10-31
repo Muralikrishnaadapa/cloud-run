3f726f63caf249f001c3b988527c42f99f4e4639
Muralikrishnaadapa <142293106+Muralikrishnaadapa@users.noreply.github.com>
2025-10-31 22:48:49 +0530

    Fix server log message formatting

--- index.js
+++ index.js
@@ -44,4 +44,6 @@ app.get("*", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(⁠ ✅ Server running on port ${PORT} ⁠));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+
+module.exports = app;
