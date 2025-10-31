21f7d5f37f3e1250237e5668554c2c9948487a01
Muralikrishnaadapa <142293106+Muralikrishnaadapa@users.noreply.github.com>
2025-10-31 22:42:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => {
-  console.log('Incoming host:', req.headers.host);
-  res.send('✅ Cloud Run is working!');
-});
+app.get('/', (req, res) => res.send('OK'));
+app.get('*', (req, res) => res.send('Hello from Cloud Run'));
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(⁠ Server started on port ${PORT} ⁠));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(⁠ ✅ Server running on port ${PORT} ⁠);
+});
